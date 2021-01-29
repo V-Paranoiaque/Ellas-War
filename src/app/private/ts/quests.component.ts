82cc7afa8ef7951f1f3660260e102e2a7faf1f26
Mighty <mighty@ellaswar.com>
2021-01-29 21:34:28 +0100

    Start quests page

--- src/app/private/ts/quests.component.ts
+++ src/app/private/ts/quests.component.ts
@@ -1,8 +1,44 @@
 import { Component } from '@angular/core';
+import { Socket } from '../../../services/socketio.service';
+import { User } from '../../../services/user.service';
 
 @Component({
   templateUrl: '../html/quests.component.html',
   styleUrls: ['../css/quests.component.css']
 })
 
-export class Quests {}
+export class Quests {
+  
+  private myQuestList:any;
+  
+  constructor(private socket: Socket, public user: User) {
+    
+    this.socket.socket.on('myQuestList', (data:any) => {
+      this.myQuestList = data;
+    });
+    this.socket.socket.on('myQuestListRefresh', () => {
+      this.socket.emit('myQuestList');
+    });
+  }
+  
+  ngOnInit() {
+    setTimeout(()=> {
+      this.socket.emit("questCheck");
+      this.socket.emit("statsTmp");
+      this.socket.emit('myQuestList');
+      
+      this.socket.emit("altarConditionDivineunits");
+      this.socket.emit("altarConditionPrometheus");
+      this.socket.emit("altarConditionHestia");
+      this.socket.emit("altarConditionGaia");
+    });
+  }
+  
+  getMyQuestList() {
+    return this.myQuestList;
+  }
+  
+  myQuestValidate(id:number) {
+    this.socket.emit('myQuestValidate', id);
+  }
+}
