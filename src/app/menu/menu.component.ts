4cac462c32d3a005762a0e27dba8a2cbc8ff4df2
Mighty <mighty@ellaswar.com>
2021-02-13 18:30:52 +0100

    Start right menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -2,10 +2,12 @@ import { PublicBottomBar } from './ts/public-bottom-bar.component';
 import { CommonTopBar } from './ts/common-top-bar.component';
 import { ConnectedTopBar } from './ts/connected-top-bar.component';
 import { PublicTopBar } from './ts/public-top-bar.component';
+import { ConnectedRightMenu } from './ts/connected-right-menu.component';
 
 export const MenuComponent = [
     CommonTopBar,
     
+    ConnectedRightMenu,
     ConnectedTopBar,
     
     PublicBottomBar,
---
+++ src/app/menu/ts/connected-right-menu.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'connected-right-menu',
+  templateUrl: '../html/connected-right-menu.component.html',
+  styleUrls: ['../css/connected-right-menu.component.css']
+})
+
+export class ConnectedRightMenu {}
