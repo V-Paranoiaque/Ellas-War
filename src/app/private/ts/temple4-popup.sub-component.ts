910c9b5dbb7b94f32458d3c9eaa04b0932ea5bbf
Mighty <mighty@ellaswar.com>
2021-02-28 20:59:43 +0100

    End of level quests + temples 3 & 4

--- src/app/private/ts/temple4-popup.sub-component.ts
+++ src/app/private/ts/temple4-popup.sub-component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Socket } from '../../../services/socketio.service';
+import { TranslateService } from '@ngx-translate/core';
 import { User } from '../../../services/user.service';
 
 @Component({
@@ -10,6 +11,38 @@ import { User } from '../../../services/user.service';
 
 export class Temple4Popup {
   
-  constructor(private socket: Socket, public user: User) {
+  public temple:number;
+  public price:any;
+  
+  constructor(private socket: Socket, public user: User, public translate: TranslateService) {
+    this.temple = 0;
+    this.price = [
+      {'resource': 'drachma', 'quantity': 4000000 },
+      {'resource': 'wood', 'quantity': 25000000 },
+      {'resource': 'stone', 'quantity': 1600000 },
+      {'resource': 'marble', 'quantity': 160000 },
+      {'resource': 'gold', 'quantity': 120000 }
+    ];
+  }
+  
+  setTemple(id:number) {
+    if(id == this.temple) {
+      this.temple = 0;
+    }
+    else {
+      this.temple = id;
+    }
+  }
+  build() {
+    this.socket.emit('buildTemple4', this.temple);
+  }
+  
+  canBuild() {
+    for(let i in this.price) {
+      if(this.user.getPropertyNb(this.price[i].resource) < this.price[i].quantity) {
+        return false;
+      }
+    }
+    return true;
   }
 }
