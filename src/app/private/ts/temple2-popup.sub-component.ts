d6e792d033d397d83c40257cd1fde06df8f85617
Mighty <mighty@ellaswar.com>
2021-02-28 19:52:26 +0100

    Temple 2

--- src/app/private/ts/temple2-popup.sub-component.ts
+++ src/app/private/ts/temple2-popup.sub-component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Socket } from '../../../services/socketio.service';
+import { TranslateService } from '@ngx-translate/core';
 import { User } from '../../../services/user.service';
 
 @Component({
@@ -10,6 +11,37 @@ import { User } from '../../../services/user.service';
 
 export class Temple2Popup {
   
-  constructor(private socket: Socket, public user: User) {
+  public temple:number;
+  public price:any;
+  
+  constructor(private socket: Socket, public user: User, public translate: TranslateService) {
+    this.temple = 0;
+    this.price = [
+      {'resource': 'drachma', 'quantity': 500000 },
+      {'resource': 'wood', 'quantity': 400000 },
+      {'resource': 'stone', 'quantity':  50000 },
+      {'resource': 'marble', 'quantity': 1250 }
+    ];
+  }
+  
+  setTemple(id:number) {
+    if(id == this.temple) {
+      this.temple = 0;
+    }
+    else {
+      this.temple = id;
+    }
+  }
+  build() {
+    this.socket.emit('buildTemple2', this.temple);
+  }
+  
+  canBuild() {
+    for(let i in this.price) {
+      if(this.user.getPropertyNb(this.price[i].resource) < this.price[i].quantity) {
+        return false;
+      }
+    }
+    return true;
   }
 }
