a36f369153915700967250961b7c50a23363da2c
Mighty <mighty@ellaswar.com>
2021-03-05 19:10:07 +0100

    Fix JS error

--- src/app/private/ts/id-to-divinebonus.sub-component.ts
+++ src/app/private/ts/id-to-divinebonus.sub-component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { User } from '../../../services/user.service';
 
 @Component({
   selector: 'id-to-divinebonus',
@@ -10,6 +11,6 @@ export class IdToDivineBonus {
   @Input()
   id: any;
   
-  constructor(public translate: TranslateService) {}
+  constructor(public user: User, public translate: TranslateService) {}
   
 }
