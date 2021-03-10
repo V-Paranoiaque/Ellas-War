8e809a646854b0e46dc24e227d9b10571afe81af
Mighty <mighty@ellaswar.com>
2021-03-10 22:53:24 +0100

    Build poseidon wall, with ress or ambrosia

--- src/app/private/ts/temple-info-popup.sub-component.ts
+++ src/app/private/ts/temple-info-popup.sub-component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Socket } from '../../../services/socketio.service';
+import { TranslateService } from '@ngx-translate/core';
 import { User } from '../../../services/user.service';
 
 @Component({
@@ -11,6 +12,30 @@ import { User } from '../../../services/user.service';
 export class TempleInfoPopup {
   @Input() temple: any;
   
-  constructor(private socket: Socket, public user: User) {
+  public wallDefense:number;
+  
+  constructor(private socket: Socket, public user: User, public translate: TranslateService) {
+    this.wallDefense = 0;
+    
+    setTimeout(() => {
+      this.socket.emit('wallDefense');
+    }, 0);
+    
+    
+    this.socket.socket.on('wallDefense', (def:number) => {
+      this.wallDefense = def;
+    });
+  }
+  
+  powerUse(powerid:number, param:number) {
+    let info = {
+      'id': powerid,
+      'param': param
+    }
+    this.socket.emit('powersUse', info);
+  }
+  
+  wallErect() {
+    this.socket.emit('wallErect');
   }
 }
