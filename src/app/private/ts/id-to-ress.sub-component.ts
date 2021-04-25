f68a73f79ee142c455319d01892478e1fac5ef4e
Mighty <mighty@ellaswar.com>
2021-04-25 14:37:27 +0200

    Add new icons for resources

--- src/app/private/ts/id-to-ress.sub-component.ts
+++ src/app/private/ts/id-to-ress.sub-component.ts
@@ -2,7 +2,8 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'id-to-ress',
-  templateUrl: '../html/id-to-ress.sub-component.html'
+  templateUrl: '../html/id-to-ress.sub-component.html',
+  styleUrls: ['../css/id-to-ress.sub-component.css']
 })
 
 export class IdToRess {
