7ad3c6088ea2fc991545c024f2b29914e21092fc
Mighty <mighty@ellaswar.com>
2021-05-03 22:09:53 +0200

    Improve help page design

--- src/app/public/ts/help.component.ts
+++ src/app/public/ts/help.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { User } from '../../../services/user.service';
 
 @Component({
-  templateUrl: '../html/help.component.html'
+  templateUrl: '../html/help.component.html',
+  styleUrls: ['../css/help.component.css']
 })
 
 export class Help {
