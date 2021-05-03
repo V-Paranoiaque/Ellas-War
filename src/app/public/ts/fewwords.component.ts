096bc984370965ea5fc70554e678fc1322d775f8
Mighty <mighty@ellaswar.com>
2021-05-03 19:27:09 +0200

    Add description pages

--- src/app/public/ts/fewwords.component.ts
+++ src/app/public/ts/fewwords.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { User } from '../../../services/user.service';
+
+import facebookIcon from '@iconify-icons/logos/facebook';
+import githubOctocat from '@iconify-icons/logos/github-octocat';
 
 @Component({
   templateUrl: '../html/fewwords.component.html'
 })
 
 export class FewWords {
-
+  facebookIcon  = facebookIcon;
+  githubOctocat = githubOctocat;
+  
+  constructor(public user: User) {
+    
+  }
 }
