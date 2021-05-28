4f940ae85052fe8974f527542229245f9dfa0702
Mighty <mighty@ellaswar.com>
2021-05-29 00:18:39 +0200

    Fix Google connection on mobile

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -322,6 +322,21 @@ export class User {
   }
   
   oauthGoogle() {
-    this.oauthService.initImplicitFlow();
+    if(environment.mobile == 0) {
+      this.oauthService.initImplicitFlow();
+    }
+    else {
+      let w:any = window;
+       w['plugins'].googleplus.login({
+        'scopes': 'profile email', // optional, space-separated list of scopes, If not included or empty, defaults to `profile` and `email`.
+        'webClientId': '516434350883-uqounu7irl3u2jl42dph661dsqg0vvnj.apps.googleusercontent.com' // optional clientId of your Web application from Credentials settings of your project - On Android, this MUST be included to get an idToken. On iOS, it is not required.
+      },
+      (obj:any) => {
+        this.socket.emit('mobileGoogle', {'token': obj.idToken});
+      },
+      (msg:any) => {
+        alert('error: ' + msg);
+      });
+    }
   }
 }
