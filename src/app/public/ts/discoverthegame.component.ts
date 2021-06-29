1e3df8210d751e5f72ea7cbdfd44e6bd6c2a6941
Mighty <mighty@ellaswar.com>
2021-06-29 20:19:27 +0200

    Add page titles and translations

--- src/app/public/ts/discoverthegame.component.ts
+++ src/app/public/ts/discoverthegame.component.ts
@@ -36,6 +36,60 @@ export class DiscoverTheGame {
           this.titleService.setTitle(res);
         });
       break;
+      
+      case 'constructions':
+        this.translate.get('Construct buildings to make up your Greek city').subscribe((res: string) => {
+          this.titleService.setTitle(res);
+        });
+      break;
+      
+      case 'treasure':
+        this.translate.get('Hide your drachmas in your treasure').subscribe((res: string) => {
+          this.titleService.setTitle(res);
+        });
+      break;
+      
+      case 'temples':
+        this.translate.get('Build temples and enjoy the powers of the Gods').subscribe((res: string) => {
+          this.titleService.setTitle(res);
+        });
+      break;
+      
+      case 'recruitment':
+        this.translate.get('Recruit your army to defend your city from invaders').subscribe((res: string) => {
+          this.titleService.setTitle(res);
+        });
+      break;
+      
+      case 'army':
+        this.translate.get('Build a varied army and make your strategy').subscribe((res: string) => {
+          this.titleService.setTitle(res);
+        });
+      break;
+      
+      case 'attack':
+        this.translate.get('Build your offensive strategy to attack other cities').subscribe((res: string) => {
+          this.titleService.setTitle(res);
+        });
+      break;
+      
+      case 'defense':
+        this.translate.get('Defend your city with your army and your towers').subscribe((res: string) => {
+          this.titleService.setTitle(res);
+        });
+      break;
+      
+      case 'trade':
+        this.translate.get('Buy the resources you need').subscribe((res: string) => {
+          this.titleService.setTitle(res);
+        });
+      break;
+      
+      case 'sell':
+        this.translate.get('Sell resources you do not need').subscribe((res: string) => {
+          this.titleService.setTitle(res);
+        });
+      break;
     }
 
   }
