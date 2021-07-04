10d7a4164f568aef7ced6b17996f910a112a2c30
Mighty <mighty@ellaswar.com>
2021-07-04 16:23:58 +0200

    Configure SQ coverage tests

--- karma.conf.js
+++ karma.conf.js
@@ -25,20 +25,21 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ew'),
+      dir: require('path').join(__dirname, './coverage'),
       subdir: '.',
       reporters: [
         { type: 'html' },
+        { type: 'lcov' },
         { type: 'text-summary' }
       ]
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['coverage', 'progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Firefox'],
-    singleRun: false,
+    singleRun: true,
     restartOnFileChange: true
   });
 };
