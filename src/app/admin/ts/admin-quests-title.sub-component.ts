da27400fe43c1c88a06cf361ad01f2b20b303482
Mighty <mighty@ellaswar.com>
2021-07-05 16:37:12 +0200

    Format numbers

--- src/app/admin/ts/admin-quests-title.sub-component.ts
+++ src/app/admin/ts/admin-quests-title.sub-component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'admin-quests-title',
@@ -7,4 +8,8 @@ import { Component, Input } from '@angular/core';
 
 export class AdminQuestsTitle {
   @Input() quest:any;
+  
+  constructor(public translate: TranslateService) {
+    
+  }
 }
