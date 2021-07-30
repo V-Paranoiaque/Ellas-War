0df523260a7654e64c7004e558634a208f2d6220
Mighty <mighty@ellaswar.com>
2021-07-30 21:39:28 +0200

    Fix local linux build

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -1,5 +1,5 @@
 docker run --rm -ti -v $(pwd):/root/ew -w /root/ew \
-           -v $(pwd)/build-linux.json:/root/ew/build.json \
+           -v $(pwd)/res/electron/build-pipeline-linux.json:/root/ew/build.json \
            -ti ew-build \
            sh -c "npm install && \
                   node_modules/.bin/ng build --configuration=mobile && \
