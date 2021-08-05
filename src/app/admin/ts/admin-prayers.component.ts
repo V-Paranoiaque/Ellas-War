7311de6bdfa503ea98d8bfe746012c07e3295ef7
Mighty <mighty@ellaswar.com>
2021-08-05 21:57:46 +0200

    Admin page to manage prayers

--- src/app/admin/ts/admin-prayers.component.ts
+++ src/app/admin/ts/admin-prayers.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
+import { Socket } from '../../../services/socketio.service';
 import { TranslateService } from '@ngx-translate/core';
 import { User } from '../../../services/user.service';
+
+import plusIcon from '@iconify/icons-bi/plus';
 
 @Component({
   templateUrl: '../html/admin-prayers.component.html',
@@ -9,11 +12,52 @@ import { User } from '../../../services/user.service';
 
 export class AdminPrayers {
   
-  constructor(public user: User, public translate: TranslateService) {
+  public adminPrayersList:any;
+  public request:any;
+  
+  plusIcon = plusIcon;
+  
+  constructor(private socket: Socket, public user: User, public translate: TranslateService) {
+    this.adminPrayersList = [];
+    this.request = {
+      'quantity': '',
+      'player': '',
+      'type': 0
+    };
   }
   
   ngOnInit() {
     this.user.checkPermissions([1]);
+    
+    this.socket.emit('adminPrayersList');
+    
+    this.socket.on('adminPrayersList', (data:any) => {
+      this.adminPrayersList = data;
+    });
+    this.socket.on('adminPrayersRefresh', () => {
+      this.socket.emit('adminPrayersList');
+    });
+  }
+  
+  ngOnDestroy() {
+    this.socket.removeListener('adminPrayersList');
+    this.socket.removeListener('adminPrayersRefresh');
+  }
+  
+  prayersAsk() {
+    this.socket.emit('adminPrayersAsk', {
+      'target': this.request.player,
+      'nb': this.request.quantity,
+      'type': this.request.type
+    });
+    this.request = {
+      'quantity': '',
+      'player': '',
+      'type': 0
+    };
+  }
+  
+  prayersValide(id:number, result:number) {
+    this.socket.emit('adminPrayersValide', {'id': id, 'result': result});
   }
 }
-  
