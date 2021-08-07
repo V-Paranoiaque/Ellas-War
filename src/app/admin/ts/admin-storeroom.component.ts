f24dd8531de75b068d6fba1a67e7e54e046e8d1d
Mighty <mighty@ellaswar.com>
2021-08-07 18:21:25 +0200

    Start admin storeroom page

--- src/app/admin/ts/admin-storeroom.component.ts
+++ src/app/admin/ts/admin-storeroom.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Socket } from '../../../services/socketio.service';
 import { TranslateService } from '@ngx-translate/core';
 import { User } from '../../../services/user.service';
 
@@ -9,11 +10,119 @@ import { User } from '../../../services/user.service';
 
 export class AdminStoreroom {
   
-  constructor(public user: User, public translate: TranslateService) {
+  public list:any[];
+  public msgPageNb:any;
+  public currentPage:any;
+  
+  public begining:string;
+  public beginDate:any;
+  public minRate:string;
+  public maxRate:string;
+  public player1:string;
+  public player2:string;
+  public resource:any;
+  public sort:string;
+  public still:any;
+  public solded:any;
+  
+  constructor(public user: User, private socket: Socket,
+              public translate: TranslateService) {
+    this.list = [];
+    this.msgPageNb = 1;
+    this.currentPage = 1;
+    
+    this.begining = '';
+    this.minRate = '';
+    this.maxRate = '';
+    this.player1 = '';
+    this.player2 = '';
+    this.resource = '';
+    this.sort = '';
   }
   
   ngOnInit() {
     this.user.checkPermissions([1]);
+    
+    this.begining = this.researchDate();
+    this.adminStoreroomList(0);
+    
+    this.socket.on('adminStoreroomList', (msg:any) => {
+      this.list      = msg.list;
+      this.msgPageNb = msg.nbPage;
+    });
+  }
+  
+  ngOnDestroy() {
+    this.socket.removeListener('adminStoreroomList');
+  }
+  
+  range(a:number, b:number) {
+    let list = []
+    for(a;a<=b;a++) {
+      list.push(a);
+    }
+    return list;
+  }
+  
+  researchDate() {
+    let date = new Date();
+    date.setDate(date.getDate()-7);
+    return date.getDate() + '/' + (date.getMonth()+1) + '/' + date.getFullYear();
+  }
+  
+  adminStoreroomChange(page:any) {
+    this.currentPage = page;
+    this.socket.emit('adminStoreroomList', {
+      'minRate': this.minRate,
+      'maxRate': this.maxRate,
+      'begining': this.begining,
+      'still':    this.still,
+      'solded':   this.solded,
+      'resource': this.resource,
+      'player1':  this.player1,
+      'player2':  this.player2,
+      'sort':     this.sort,
+      'page':     this.currentPage
+    });
+  }
+  
+  adminStoreroomList(reset:number) {
+    if(reset == 1) {
+      this.currentPage = 1;
+      this.msgPageNb = 1;
+    }
+    var beginDate;
+    if(this.begining != '') {
+      beginDate = this.begining.split('/');
+      beginDate = parseInt((new Date(beginDate[1]+"/"+beginDate[0]+"/"+beginDate[2]).getTime() / 1000).toFixed(0))
+    }
+    else {
+      beginDate = this.begining;
+    }
+    console.log({
+      'minRate': this.minRate,
+      'maxRate': this.maxRate,
+      'begining': this.beginDate,
+      'still':    this.still,
+      'solded':   this.solded,
+      'resource': this.resource,
+      'player1':  this.player1,
+      'player2':  this.player2,
+      'sort':     this.sort,
+      'page':     this.currentPage
+    });
+    this.socket.emit('adminStoreroomList', {
+      'minRate': this.minRate,
+      'maxRate': this.maxRate,
+      'begining': this.beginDate,
+      'still':    this.still,
+      'solded':   this.solded,
+      'resource': this.resource,
+      'player1':  this.player1,
+      'player2':  this.player2,
+      'sort':     this.sort,
+      'page':     this.currentPage
+    });
   }
 }
   
