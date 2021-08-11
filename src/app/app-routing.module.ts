e2b78a8d9eaf8b8db9d20a106827dbf9bb3cc83d
Mighty <mighty@ellaswar.com>
2021-08-11 19:14:32 +0200

    Preload modules to make the game faster

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 //Public
 import { Auth } from './public/ts/auth.component';
@@ -162,7 +162,9 @@ routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadAllModules
+    })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
