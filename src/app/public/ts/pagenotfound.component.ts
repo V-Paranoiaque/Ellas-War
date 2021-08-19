210f576c951bdaa6a6014631286d0c0074430f49
Mighty <mighty@ellaswar.com>
2021-08-19 18:49:14 +0200

    Send 404 to the server for stats

--- src/app/public/ts/pagenotfound.component.ts
+++ src/app/public/ts/pagenotfound.component.ts
@@ -1,7 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router'
+import { Socket } from '../../../services/socketio.service';
 
 @Component({
   templateUrl: '../html/pagenotfound.component.html'
 })
 
-export class PageNotFound {}
+export class PageNotFound {
+  
+  constructor(private router: Router, private socket: Socket) {
+    
+  }
+  
+  ngOnInit() {
+    if(this.router.url != '/404-test') {
+      this.socket.emit('404', this.router.url);
+    }
+  }
+}
