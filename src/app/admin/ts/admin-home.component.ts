f2cd9332ab7dbc73a673e1b23b670ba7a178f1d8
Mighty <mighty@ellaswar.com>
2021-10-11 19:31:33 +0200

    Display general information on the admin home page

--- src/app/admin/ts/admin-home.component.ts
+++ src/app/admin/ts/admin-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Socket } from '../../../services/socketio.service';
 import { TranslateService } from '@ngx-translate/core';
 import { User } from '../../../services/user.service';
@@ -11,16 +12,25 @@ import { User } from '../../../services/user.service';
 export class AdminHomeComponent implements OnInit, OnDestroy {
   
   public adminStats:any;
+  public apiInfo:any;
   
-  constructor(private socket: Socket, public user: User, 
-              public translate: TranslateService) {
+  private sub:any;
+  
+  constructor(protected http: HttpClient, private socket: Socket,
+              public user: User, public translate: TranslateService) {
     this.adminStats = {};
+    this.apiInfo = {};
   }
   
   ngOnInit() {
     this.user.checkPermissions([1]);
     
     this.socket.emit('adminStats');
+    
+    let url = this.socket.url+'/api.json';
+    this.sub = this.http.get(url).subscribe((result:any) => {
+      this.apiInfo = result;
+    });
     
     this.socket.on('adminStats', (msg:any) => {
       this.adminStats = msg;
@@ -29,5 +39,9 @@ export class AdminHomeComponent implements OnInit, OnDestroy {
   
   ngOnDestroy() {
     this.socket.removeListener('adminStats');
+    
+    if(this.sub) {
+      this.sub.unsubscribe();
+    }
   }
 }
