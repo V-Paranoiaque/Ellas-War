a99c6d69c39d2cc9a68eff6d3a5ecd65c1dc1a5d
Mighty <mighty@ellaswar.com>
2021-10-19 22:51:48 +0200

    Start admin-profile tests

---
+++ src/app/admin/ts/admin-profile.component.spec.ts
@@ -0,0 +1,60 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Socket } from '../../../services/socketio.service';
+import { User } from '../../../services/user.service';
+import { OAuthModule, OAuthService } from 'angular-oauth2-oidc';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { IconModule } from '@visurel/iconify-angular';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+import { MenuComponent } from '../../menu/menu.component';
+import { AdminProfileComponent } from './admin-profile.component';
+
+describe('AdminPrayersComponent', () => {
+  let socket: Socket;
+  
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        MenuComponent,
+        AdminProfileComponent,
+      ],
+      imports: [
+        RouterTestingModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: httpTranslateLoader,
+            deps: [HttpClient]
+          }
+        }),
+        OAuthModule.forRoot(),
+        HttpClientTestingModule,
+        IconModule,
+      ],
+      providers: [
+        Socket, User, OAuthService,
+        BsModalService, FormBuilder, FormGroup
+      ],
+    }).compileComponents();
+    socket = TestBed.inject(Socket);
+    socket.setupSocketConnection('dev.ellaswar.com');
+  });
+  
+  it('should create the service', () => {
+    const fixture = TestBed.createComponent(AdminProfileComponent);
+    fixture.detectChanges();
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+  
+});
+
+// AOT compilation support
+export function httpTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/');
+}
