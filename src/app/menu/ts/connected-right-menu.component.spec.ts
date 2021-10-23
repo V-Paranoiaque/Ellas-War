2dd9e8ef451d73ce2e73ed4faf15668f992b489f
Mighty <mighty@ellaswar.com>
2021-10-23 18:50:16 +0200

    Start tests for bar components

---
+++ src/app/menu/ts/connected-right-menu.component.spec.ts
@@ -0,0 +1,62 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Socket } from '../../../services/socketio.service';
+import { User } from '../../../services/user.service';
+import { OAuthModule, OAuthService } from 'angular-oauth2-oidc';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { BsModalService } from 'ngx-bootstrap/modal';
+import { IconModule } from '@visurel/iconify-angular';
+import { FormBuilder } from '@angular/forms';
+
+import { MenuComponent } from '../../menu/menu.component';
+import { ConnectedRightMenuComponent } from './connected-right-menu.component';
+import { environment } from '../../../environments/environment';
+
+describe('ConnectedRightMenuComponent', () => {
+  let socket: Socket;
+  
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        MenuComponent,
+        ConnectedRightMenuComponent,
+      ],
+      imports: [
+        RouterTestingModule,
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: httpTranslateLoader,
+            deps: [HttpClient]
+          }
+        }),
+        OAuthModule.forRoot(),
+        HttpClientTestingModule,
+        IconModule,
+      ],
+      providers: [
+        Socket, User, OAuthService,
+        BsModalService, FormBuilder
+      ],
+    }).compileComponents();
+    socket = TestBed.inject(Socket);
+    socket.setupSocketConnection(environment.SERVER_DEV);
+  });
+  
+  it('should create the service', () => {
+    const fixture = TestBed.createComponent(ConnectedRightMenuComponent);
+    fixture.detectChanges();
+    const app = fixture.componentInstance;
+    
+    expect(app).toBeTruthy();
+  });
+  
+});
+
+// AOT compilation support
+export function httpTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/');
+}
